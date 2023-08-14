55bd42e963aad7b75222d3ffe1edf15494493b29
Zemrude <eufonio@qq.com>
2023-08-14 20:41:05 +0800

    no message

--- script/DisplaySystemTime.cs
+++ script/DisplaySystemTime.cs
@@ -6,5 +6,6 @@ public partial class DisplaySystemTime : Label
     public override void _Process(double delta)
     {
         this.Text = Time.GetTimeStringFromSystem();
+        Vector2 vec = new Vector2(0, 0);
     }
 }
